f0a43718f7b0ff6cf290b66b169bd478ff49d4c2
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-11-07 16:14:22 +0100

    Fix course actions

--- src/app/courses/course.actions.ts
+++ src/app/courses/course.actions.ts
@@ -1,13 +1,14 @@
-import { createAction } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
 
 // Fetch all courses from the backend: this is more a COMMAND than an event
-export loadAllCourses = createAction(
+export const loadAllCourses = createAction(
   '[Courses Resolver] Load All Courses'
 );
 
 // All course fetch finished: this is more an EVENT than a command
 export const allCourseLoaded = createAction(
-  '[Load Courses Fetched] All Course Loaded'
+  '[Load Courses Fetched] All Course Loaded',
+  props<{courses: Course[]}>()
 );
 
 
