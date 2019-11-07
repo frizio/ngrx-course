1bd92f68f77cebd1b533ffe8ef6d05c150df01ba
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-11-07 09:33:04 +0100

    Fix selectors code

--- src/app/auth/auth.selector.ts
+++ src/app/auth/auth.selector.ts
@@ -2,12 +2,12 @@ import { createSelector } from '@ngrx/store';
 
 // Selector is a MEMOIZED mapping function
 export const isLoggedIn = createSelector(
-  state => state.auth,
+  state => state['auth'],
   (auth) => !!auth.user // (auth) is the result of previous state.auth
 );
 
 
 export const isLoggedOut = createSelector(
-  isLoggedIn,
-  loggedIn => !loggedIn
+  isLoggedIn, // Mapping Function
+  loggedIn => !loggedIn // Projector function
 );
