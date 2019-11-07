b422860642602f9daab4b371d191116fd8df84bc
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-11-07 16:35:48 +0100

    Loading NgRx Entity Data using a Router Resolver

---
+++ src/app/courses/courses.resolver.ts
@@ -0,0 +1,37 @@
+import { AppState } from './../reducers/index';
+import { Observable } from 'rxjs';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { loadAllCourses } from './course.actions';
+import { first, finalize, tap } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CoursesResolver implements Resolve<any> {
+
+  loading = false;
+
+  constructor(
+    private store: Store<AppState>
+  ) { }
+
+  resolve(route: ActivatedRouteSnapshot,
+          state: RouterStateSnapshot): Observable<any> {
+    return this.store
+      .pipe(
+        tap(
+          () => {
+            if (!this.loading) {
+              this.loading = true;
+              this.store.dispatch(loadAllCourses());
+            }
+          }
+        ),
+        first(),
+        finalize(
+          () => this.loading = false
+        )
+      );
+  }
+
+}
