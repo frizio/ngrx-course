9a2950cacc4a9ffd090e2cfb9dacbe185cff8714
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-11-07 11:04:49 +0100

    Fix AuthGuard add import statements

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,12 +1,31 @@
+import { tap } from 'rxjs/operators';
+import { isLoggedIn } from './auth.selector';
+import { select, Store } from '@ngrx/store';
+import { AppState } from './../reducers/index';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
+  constructor(
+    private store: Store<AppState>,
+    private router: Router
+  ) { }
+
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return null;
+    return this.store
+              .pipe(
+                select(isLoggedIn),
+                tap(
+                  (loggedIn) => {
+                    if (!loggedIn) {
+                      this.router.navigateByUrl('/login');
+                    }
+                  }
+                )
+              );
   }
 
 }
