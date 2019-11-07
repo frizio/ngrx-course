0fa35f56ba41341b65dab9f3bc58390a48baebfc
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-11-07 11:48:21 +0100

    (Side) Effect: second implementation

--- src/app/auth/auth.effect.ts
+++ src/app/auth/auth.effect.ts
@@ -1,5 +1,7 @@
+import { tap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import { Actions } from '@ngrx/effects';
+import { Actions, ofType } from '@ngrx/effects';
+import { AuthActions } from './actions-type';
 
 @Injectable()
 export class AuthEffects {
@@ -8,17 +10,23 @@ export class AuthEffects {
     // Use to notify whenever action get triggered
     private actions$: Actions
   ) {
-    actions$.subscribe(
-      (action) => {
-        if (action.type === '[Login Page] User Login') {
-          // The side effect
-          localStorage.setItem(
-            'user',
-            JSON.stringify(action['user'])
-          );
-        }
-      }
-    );
+
+    const login$ = this.actions$
+      .pipe(
+        ofType(AuthActions.login), // filter(AuthActions.login.type)
+        tap(
+          (action) => {
+            // The side effect
+            localStorage.setItem(
+              'user',
+              JSON.stringify(action.user)
+            );
+          }
+        )
+      );
+
+    login$.subscribe();
+
   }
 
 }
